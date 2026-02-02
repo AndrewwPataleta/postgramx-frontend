66475544ec969b5077b3e485348c250af09b41c7
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-02 15:31:35 +0000

    deploy: 5358f47aea8e7e91d015e613904c620343fd2f32

---
+++ spa/assets/listingsApi-DTA4TPEn.js
@@ -0,0 +1,1 @@
+import{y as t}from"./index-D5-q7-5r.js";const i=async s=>t("/listings/create",s),a=async s=>t("/listings/update",s),c=async s=>t("/listings/by-channel",s);export{i as c,c as l,a as u};
