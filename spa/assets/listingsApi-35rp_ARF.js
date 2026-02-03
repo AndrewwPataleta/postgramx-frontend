1fdf071ff5733ec80d6d8853009c0381efbc1642
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-03 16:53:45 +0000

    deploy: ac0ad0809ca23e00c765891af3799916aff2d1b6

---
+++ spa/assets/listingsApi-35rp_ARF.js
@@ -0,0 +1,1 @@
+import{z as t}from"./index-DzwS5EPl.js";const i=async s=>t("/listings/create",s),a=async s=>t("/listings/update",s),c=async s=>t("/listings/by-channel",s);export{i as c,c as l,a as u};
