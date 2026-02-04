9a6489db00b4e7b3ee39e454c5bd9ecf9ee38421
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-04 22:13:06 +0000

    deploy: 7fa465ce0c788521451eb7549cdd0aefda97cb64

--- spa/assets/listingsApi-C7FwE5Ju.js
+++
@@ -1,1 +0,0 @@
-import{z as t}from"./index-BWtBYAzu.js";const i=async s=>t("/listings/create",s),a=async s=>t("/listings/update",s),c=async s=>t("/listings/by-channel",s);export{i as c,c as l,a as u};
