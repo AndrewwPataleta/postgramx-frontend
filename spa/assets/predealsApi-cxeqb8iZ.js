8be83bc12ff75a233f39e99aa6b2eeeb37339bbc
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-05 18:27:50 +0000

    deploy: 7ea2a9a09bb2b9c83546856231557f36f17274a3

--- spa/assets/predealsApi-cxeqb8iZ.js
+++
@@ -1,1 +0,0 @@
-import{z as a}from"./index-y4-pEd1Q.js";const r=async e=>a("/predeals/create",e),t=async e=>a("/predeals/get",e),c=async e=>{await a("/predeals/cancel",e)};export{c as a,t as b,r as p};
