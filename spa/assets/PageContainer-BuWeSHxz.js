e139d86ecb7f111df510e94ffa84142823843854
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-08 20:04:27 +0000

    deploy: 488e478e784c8484e0f1080e5b35c549605c866f

---
+++ spa/assets/PageContainer-BuWeSHxz.js
@@ -0,0 +1,1 @@
+import{j as a,n as r}from"./index-BbrmV-gD.js";function e({children:n,className:s}){return a.jsx("div",{className:r("px-4 pb-24",s),children:n})}export{e as P};
---
+++ spa/assets/useQuery-D6XAyg3Q.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./useBaseQuery-Bu6-_N3d.js";function a(e,s){return r(e,u)}export{a as u};
