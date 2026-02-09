22b280496933ba2e53f778ad106b652aa3dbd4ef
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-09 17:39:49 +0000

    deploy: 1825a420158025b5b281a21b20091c29628cba76

--- spa/assets/predealsApi-DpdCLqLW.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./index-BZMxq81o.js";const r=async e=>a("/predeals/create",e),t=async e=>a("/predeals/get",e),c=async e=>{await a("/predeals/cancel",e)};export{c as a,t as b,r as p};
--- spa/assets/useQuery-Di7wpPKL.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-D3L3a1o2.js";function a(e,s){return r(e,u)}export{a as u};
