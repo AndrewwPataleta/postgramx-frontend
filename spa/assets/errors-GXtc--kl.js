9571a1174e2090521b9d1009f850e60360099755
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-12 16:17:04 +0000

    deploy: 8e21f9f9a893ffe6cc0e3aa518e6cdd2f92491f4

--- spa/assets/errors-GXtc--kl.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./index-DdsWUhec.js";const t=(r,o,a)=>e(r,o,a);export{t as g};
--- spa/assets/useQuery-vGR70xGL.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-DXE5a8vO.js";function a(e,s){return r(e,u)}export{a as u};
