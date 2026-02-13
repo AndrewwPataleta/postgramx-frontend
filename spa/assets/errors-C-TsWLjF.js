8402c32c1e40b1ae5dbb012b39ea0935bda511b2
AndrewwPataleta <AndrewwPataleta@users.noreply.github.com>
2026-02-13 00:26:22 +0000

    deploy: 56b5c9969476d85706ad571e610632644b6e21c9

---
+++ spa/assets/errors-C-TsWLjF.js
@@ -0,0 +1,1 @@
+import{ab as e}from"./index-Cr6Idl0i.js";const t=(r,o,a)=>e(r,o,a);export{t as g};
--- spa/assets/useQuery-DnszDGE4.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-BBkqSpI7.js";function a(e,s){return r(e,u)}export{a as u};
